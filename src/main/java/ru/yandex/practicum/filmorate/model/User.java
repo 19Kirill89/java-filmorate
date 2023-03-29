d6ec8a00e2d36ebc5ade4931a17cdc1b8ada9b98
Кирилл <Stockmanrat@yandex.ru>
2023-03-29 10:55:19 +0300

    14) в душе не понимаю почему чего ему нужно, сделал старое, залил, просто дляпромежуточного тз.

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -4,8 +4,8 @@ import lombok.Data;
 
 import javax.validation.constraints.*;
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 public class User {
@@ -16,7 +16,7 @@ public class User {
             message = "Email должен быть правильным")
     private String email;
     private String name;
-    private Map<Long, Boolean> friends = new HashMap<>();
+    private Set<Long> friends = new HashSet<>();
     @NotBlank(message = "не может быть пуст")
     @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "не должно быть пробелов")
     private String login;
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exeption.NotFound;
@@ -8,7 +9,9 @@ import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @Service
@@ -39,7 +42,7 @@ public class UserService {
         return userStorage.getUserById(id);
     }
 
-    public User addFriend(long userId, long friendId) throws NotFound {
+    public User addFriends(Long userId, Long friendId) throws NotFound {
         final User user = userStorage.getUserById(userId);
         final User friend = userStorage.getUserById(friendId);
         if (friend == null || friend.getId() < 0) {
@@ -47,18 +50,14 @@ public class UserService {
         } else if (user == null || user.getId() < 0) {
             throw new NotFound("нет пользователя с id " + userId);
         } else {
-            if (friend.getFriends().containsKey(userId)) {
-                friend.getFriends().put(userId, true);
-                user.getFriends().put(friendId, true);
-            } else {
-                user.getFriends().put(friendId, false);
-            }
+            user.getFriends().add(friendId);
+            friend.getFriends().add(userId);
             userStorage.updateUser(friend);
             return userStorage.updateUser(user);
         }
     }
 
-    public User deleteFriend(long userId, long friendId) throws NotFound {
+    public User deleteFriends(Long userId, Long friendId) throws NotFound {
         User user = userStorage.getUserById(userId);
         User friend = userStorage.getUserById(friendId);
         if (user == null || user.getId() < 0) {
@@ -73,24 +72,22 @@ public class UserService {
         }
     }
 
-    public List<User> getFriendsList(long userId) throws NotFound {
+    public List<User> getFriendsList(Long userId) throws NotFound {
         User user = userStorage.getUserById(userId);
-        List<User> friendsList = new ArrayList<>();
         if (user == null || user.getId() < 0) {
             throw new NotFound("Пользователя с id" + userId + " не существует");
         } else {
+            List<User> friendsList = new ArrayList<>();
             if (user.getFriends() != null) {
-                for (Long id : user.getFriends().keySet()) {
-                    if (user.getFriends().get(id)) {
-                        friendsList.add(userStorage.getUserById(id));
-                    }
+                for (Long id : user.getFriends()) {
+                    friendsList.add(userStorage.getUserById(id));
                 }
             }
+            return friendsList;
         }
-        return friendsList;
     }
 
-    public List<User> getCommonFriendsList(long mainUser, long otherUserId) throws NotFound {
+    public List<User> getCommonFriendsList(Long mainUser, Long otherUserId) throws NotFound {
         List<User> commonFriends = new ArrayList<>();
         User user = userStorage.getUserById(mainUser);
         User otherUser = userStorage.getUserById(otherUserId);
@@ -101,18 +98,23 @@ public class UserService {
             throw new NotFound("нет пользователя с id " + otherUserId);
         } else {
             if (user.getFriends() == null) {
-                return List.of();
-            } else if (otherUser.getFriends() == null) {
-                return List.of();
-            } else {
-                for (long id : user.getFriends().keySet()) {
-                    if (otherUser.getFriends().containsKey(id)) {
+                if (otherUser.getFriends() == null) {
+                    return List.of();
+                } else {
+                    for (Long id : otherUser.getFriends()) {
                         commonFriends.add(userStorage.getUserById(id));
                     }
+                    return commonFriends;
                 }
+            } else {
+                Set<Long> duplicateFriendsUser = new HashSet<>(user.getFriends());
+                duplicateFriendsUser.retainAll(otherUser.getFriends());
+
+                for (Long id : duplicateFriendsUser) {
+                    commonFriends.add(userStorage.getUserById(id));
+                }
+                return commonFriends;
             }
         }
-        return commonFriends;
     }
 }
-
