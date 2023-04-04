ff85486d4292c9619c66e16f4eb9c73620a57b83
Кирилл <Stockmanrat@yandex.ru>
2023-04-05 00:20:06 +0300

    попраил пробел

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFound.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotFound.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class NotFound extends RuntimeException{
-    public NotFound(String message){
+public class NotFound extends RuntimeException {
+    public NotFound(String message) {
         super(message);
     }
 }
